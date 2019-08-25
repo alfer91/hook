129eaf354e70cfdfe8d7e3791ed1db47e242f222
alfer91 <dasungwkd@naver.com>
2019-08-25 16:23:43 +0900

    hook : useState
    differences  between react hook and react

--- src/index.js
+++ src/index.js
@@ -3,37 +3,6 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
-    // console.log(event.target);
-  };
-  return { value, onChange };
-};
-
-const App = () => {
-  // const maxLen = value => value.length <= 10;
-  const maxLen = value => !value.includes("@");
-  const name = useInput("Mr.", maxLen);
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
-    </div>
-  );
-};
-
-/* 
 // ----useState
 const App = () => {
   const [item, setItem] = useState(1);
@@ -82,6 +51,6 @@ class AppUgly extends React.Component {
     });
   };
 }
- */
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
