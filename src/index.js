a5984074541c48835d503f5c79aa6c9f55440085
alfer91 <dasungwkd@naver.com>
2019-08-26 14:34:08 +0900

    useFullscreen
    use function requestFullscreen in element.
    use function exitFullscreen in document.
    the methods are different depend on browser.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,58 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useScroll = () => {
-  const [state, setState] = useState({
-    x: 0,
-    y: 0
-  });
-  const onScroll = () => {
-    // console.log(`y ${window.scrollY}, x ${widnow.scrollX}`);
-    setState({ y: window.scrollY, x: window.scrollX });
+const useFullscreen = callback => {
+  const element = useRef();
+  const runCb = isFull => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
   };
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => window.addEventListener("scroll", onScroll);
-  }, []);
-  return state;
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      }
+      runCb(true);
+    }
+    setTimeout(exitFull, 3000);
+  };
+  const exitFull = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
 };
 
 const App = () => {
-  const { y } = useScroll();
+  const onFullS = isFull => {
+    console.log(isFull ? "We are full" : "We are small");
+  };
+  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+      <div>
+        <img
+          ref={element}
+          src="https://stackhouse.s3.amazonaws.com/069eaa748ce02470524133acf7ee9ad1_image.png"
+        />
+        <button onClick={exitFull}>Exit fullscreen</button>
+      </div>
+      <button onClick={triggerFull}>Make fullscreen</button>
     </div>
   );
 };
